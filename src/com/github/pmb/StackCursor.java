60e08b1a6f3f47b4c46dac5307f4c01efd06823b
Simon Jackson (Netbook) <jackokring@gmail.com>
2013-02-16 08:27:22 +0000

    The StackToken is complete, now for TableToken.

---
+++ src/com/github/pmb/StackCursor.java
@@ -0,0 +1,21 @@
+package com.github.pmb;
+
+public class StackCursor extends Token {
+
+	public Token read() {
+		if(inside == empty) {
+			inside = next();
+		} else {
+			inside = inside.next();
+		}
+		return inside.read();
+	}
+	
+	public void write(Token t) {
+		throw new MachineException();
+	}
+	
+	public int hashCode() {
+		return ((Object)this).hashCode();
+	}
+}
--- src/com/github/pmb/StackToken.java
+++ src/com/github/pmb/StackToken.java
@@ -1,19 +1,23 @@
 package com.github.pmb;
 
 public class StackToken extends Token {
+	
+	public StackToken() {
+		inside = new StackCursor();
+	}
 
 	//double indirect cos different tokens must point to same stack.
 	public Token read() {
 		Token stack = inside;
 		Token top = stack.next();
 		stack.before(top.next());
-		return top;
+		return top.read();//from container token
 	}
 	
 	public void write(Token t) {
+		(t = new Token()).write(t);
 		t.before(inside.next());
 		inside.before(t);
 	}
-	
-	//hash of stack individuality not content.
+
 }
